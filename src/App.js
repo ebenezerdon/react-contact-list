8d1eea80b79feacc7fa7c7ae46034bcfc7d405e2
Ebenezer Don <ebenezerdonu@gmail.com>
2021-04-11 17:42:41 +0100

    fetch and map through contact card data  - add ContactCards component  - add use react-fetch-hook to dependencies

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
+import useFetch from 'react-fetch-hook'
+import ContactCards from './ContactCards'
+
 const App = () => {
+  const url = 'https://randomuser.me/api/'
+  const { isLoading, data, error } = useFetch(url+'?results=200')
+  console.log(data)
+
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 bg-gray-100 p-20">
-      <figure className="bg-white text-white h-80 rounded-lg shadow-md">
-        <img alt="user" className="w-32 h-32 rounded-full mx-auto mt-7" src="https://randomuser.me/api/portraits/women/40.jpg" />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">Sarah Cooper</p>
-          <p className="text-gray-500">
-            <span className="font-medium">email: </span>sarah.cooper@example.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone: </span>(694)-420-2133
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city: </span>Texas
-          </p>
-        </figcaption>
-      </figure>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+    <div className={"bg-gray-100 pt-3"}>
+      <section>
+        <form>
+          <input type={"text"} placeholder={"type keyword to search..."}/>
+        </form>
+      </section>
+      <section className={"grid sm:grid-cols-2 md:grid-cols-4 gap-6 p-20"}>
+        <ContactCards data={data}/>
+      </section>
     </div>
   )
 }
---
+++ src/ContactCards.jsx
@@ -0,0 +1,27 @@
+const ContactCards = ({data}) => {
+  return (
+    <>
+      {data?.results?.slice(0, 10).map((contact, index) => (
+        <figure className="bg-white text-white h-80 rounded-lg shadow-md" key={index}>
+          <img alt="user" className="w-32 h-32 rounded-full mx-auto mt-7" src={contact.picture.large} />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl mb-2">
+              {contact.name.first} {contact.name.last}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">email: </span>{contact.email}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">phone: </span>{contact.cell}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">city: </span>{contact.location.city}
+            </p>
+          </figcaption>
+        </figure>
+      ))}
+    </>
+  )
+}
+
+export default ContactCards
