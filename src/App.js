4d8b7dbfee45df46d5490f1c20638e0fa4037ea9
Ebenezer Don <ebenezerdonu@gmail.com>
2021-04-11 14:19:05 +0100

    style contact cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 bg-gray-100 p-20">
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
