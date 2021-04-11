81c8a6a9bc9678ae65b53389e95e9cd6022c1c85
Ebenezer Don <ebenezerdonu@gmail.com>
2021-04-11 14:46:10 +0100

    add photo to card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 bg-gray-100 p-20">
-      <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
+      <div className="bg-white text-white h-80 rounded-lg shadow-md">
+        <img alt="user" className="rounded-full h-32 mx-auto m-5" src="https://randomuser.me/api/portraits/women/20.jpg" />
+      </div>
       <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
       <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
       <div className="bg-white text-center text-white h-80 rounded-lg shadow-md">1</div>
