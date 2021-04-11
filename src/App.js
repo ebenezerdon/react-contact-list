bf61e85ad17c89fd25c8ae3bf1544831a86cf8de
Ebenezer Don <ebenezerdonu@gmail.com>
2021-04-11 15:33:43 +0100

    update margin top style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ const App = () => {
   return (
     <figure className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 bg-gray-100 p-20">
       <div className="bg-white text-white h-80 rounded-lg shadow-md">
-        <img alt="user" className="w-32 h-32 rounded-full mx-auto m-5" src="https://randomuser.me/api/portraits/women/40.jpg" />
-        <figcaption className="text-center">
+        <img alt="user" className="w-32 h-32 rounded-full mx-auto mt-7" src="https://randomuser.me/api/portraits/women/40.jpg" />
+        <figcaption className="text-center mt-5">
           <p className="text-gray-700 font-semibold text-xl mb-2">Sarah Cooper</p>
           <p className="text-gray-500">
             <span className="font-medium">email: </span>ida.gardner@example.com
