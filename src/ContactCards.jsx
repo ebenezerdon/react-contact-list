24b54b505a47423377a6b818cca069c8ef56cdc1
Ebenezer Don <ebenezerdonu@gmail.com>
2021-04-21 10:56:26 +0100

    add animation for contact cards

--- src/ContactCards.jsx
+++ src/ContactCards.jsx
@@ -1,8 +1,20 @@
-const ContactCards = ({ contactList }) => {
-  return (
+import { motion } from 'framer-motion'
+
+const ContactCards = ({ contactList, setSelectedContact }) => {
+return (
     <>
       {contactList?.map((contact, index) => (
-        <figure className="bg-white text-white h-80 rounded-lg shadow-md" key={index}>
+        <motion.button
+          initial={{ opacity: 0 }}
+          animate={{ opacity: [0, 1] }}
+          transition={{ duration: index/5 }}
+          drag={true}
+          dragElastic={1}
+          dragConstraints={{ top: 1, bottom: 1, right: 1, left: 1 }}
+          className="bg-white text-white h-80 rounded-lg shadow-md"
+          key={index}
+          onClick={() => setSelectedContact(contact)}
+        >
           <img alt="user" className="w-32 h-32 rounded-full mx-auto mt-7" src={contact.picture.large} />
           <figcaption className="text-center mt-5">
             <p className="text-gray-700 font-semibold text-xl mb-2">
@@ -18,7 +30,7 @@ const ContactCards = ({ contactList }) => {
               <span className="font-medium">city: </span>{contact.location.city}
             </p>
           </figcaption>
-        </figure>
+        </motion.button>
       ))}
     </>
   )
